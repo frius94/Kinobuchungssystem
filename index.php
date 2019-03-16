3aefb87f3c38c4e9c3b2fcc1cedba54fdaa30e29
frius94 <daniel230994@gmail.com>
2019-03-16 03:05:57 +0100

    [TASK] added background video, improved design on index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
-<body style="background-image: url('media/background3.jpg'); background-repeat: no-repeat; background-size: cover">
+<body>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <a class="navbar-brand" href="#">Kino</a>
@@ -21,46 +21,54 @@
     </div>
 </nav>
 
+<video autoplay muted loop id="bgvideo" style="
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  z-index: -1;
+  min-width: 100%;
+  min-height: 100%;">
+    <source src="media/bgvideo.mp4" type="video/mp4">
+</video>
+
 <div class="container">
     <div class="row">
         <div class="col">
-            <div class="jumbotron">
-                <h1 class="display-4">Willkommen zum TBZ Kino</h1>
-                <p class="lead">In unserem Kino laufen die aktuellsten und besten Filme! Zögern Sie nicht und lassen Sie
-                    sich von der Vielfalt an Filmen überraschen.</p>
+            <div class="jumbotron bg-dark mt-4">
+                <h1 class="display-3 text-white">Willkommen im TBZ Kino</h1>
+                <p class="lead text-white">In unserem Kino laufen die aktuellsten und besten Filme! Zögern Sie nicht und lassen Sie
+                    sich von der Vielfalt an Filmen überraschen. Für eine Reservation müssen Sie bei uns keinen neuen
+                    Account erstellen. Sie können ohne grossen
+                    Aufwand
+                    Ihren Film reservieren</p>
                 <hr class="my-4">
-                <p>Für eine Reservation ist keine separate Registrierung erforderlich. Sie können ohne grossen Aufwand
-                    Ihren Film reservieren.</p>
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col">
-            <div id="carouselExampleIndicators" class="carousel slide mb-5" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                </ol>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="media/01.jpg" class="d-block w-100">
+                <h2 class=" my-4 display-4 text-white">Demnächst im Kino</h2>
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="media/01.jpg" class="d-block w-100">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="media/02.jpg" class="d-block w-100">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="media/03.jpg" class="d-block w-100">
+                        </div>
                     </div>
-                    <div class="carousel-item">
-                        <img src="media/02.jpg" class="d-block w-100">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="media/03.jpg" class="d-block w-100">
-                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
                 </div>
-                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
             </div>
         </div>
     </div>

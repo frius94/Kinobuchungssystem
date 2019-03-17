2be629dbf7fea2b8e2d020470fcccc89433fbd3c
frius94 <daniel230994@gmail.com>
2019-03-17 12:29:21 +0100

    [TASK] improved code quality

--- reservation.php
+++ reservation.php
@@ -87,17 +87,17 @@
                 $mysqli = connectDB();
 
                 $countSeatsQuery = "SELECT count(seat.idseat) FROM seat INNER JOIN danie298_kinobuchung.row ON danie298_kinobuchung.row.idrow = seat.row_idrow INNER JOIN room ON room.idroom = danie298_kinobuchung.row.room_idroom WHERE room.idroom = " . $_GET['room'] . ";";
-                $countSeats = $mysqli->query($countSeatsQuery);
-                $countSeats = $countSeats->fetch_array()[0];
+                $countSeats = $mysqli->query($countSeatsQuery)->fetch_array()[0];
 
                 foreach (range('A', 'F') as $c) {
                     echo "<tr>
-        <th class='align-middle'>$c</th>";
+                            <th class='align-middle'>$c</th>";
+
                     for ($seatNumber = 1; $seatNumber <= 10; $seatNumber++) {
                         $seatOccupiedQuery = "SELECT seat.occupied FROM seat INNER JOIN danie298_kinobuchung.row ON danie298_kinobuchung.row.idrow = seat.row_idrow INNER JOIN room ON room.idroom = danie298_kinobuchung.row.room_idroom INNER JOIN danie298_kinobuchung.show ON show.room_idroom = room.idroom INNER JOIN reservation ON reservation.show_idshow = danie298_kinobuchung.show.idshow WHERE danie298_kinobuchung.show.idshow = " . $_GET['showid'] . " AND danie298_kinobuchung.row.row_letter = '" . $c . "' AND seat.seatnumber = " . $seatNumber . ";";
-                        $occupied = $mysqli->query($seatOccupiedQuery);
-                        $occupied = $occupied->fetch_array();
+                        $occupied = $mysqli->query($seatOccupiedQuery)->fetch_array();
                         $id = $c . $seatNumber;
+
                         if ($occupied['occupied']) {
                             echo "<td><a href='#'><img id='$id' src='media/seatGray.png' class='img-fluid' alt='seat' onclick='fillSeat(this)'></a>" . $seatNumber . "</td>";
                         } else {
